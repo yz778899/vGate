43fcf02f929ae9e182c5f398670db3c5c89ba6b7
14132465@qq.com <14132465@qq.com>
2026-04-02 02:33:48 +0800

    ——

---
+++ simple/server/handler/game_list.go
@@ -0,0 +1,43 @@
+package msg_handler
+
+import (
+	"github.com/14132465/vGate/net/app"
+	"github.com/14132465/vGate/net/handler"
+	"github.com/14132465/vGate/simple/server/msg"
+	"github.com/gofiber/fiber/v2/log"
+)
+
+// 游戏列表处理器
+type GameListHandler struct {
+	handler.BaseMsgHandler
+	Request *msg.GameListRequest
+}
+
+// 处理前
+func (this *GameListHandler) BeforeProcess() error {
+	//解码得到请求消息体
+	this.Request = &msg.GameListRequest{}
+	err := msg.Decoder(this.Msg, this.Request)
+	return err
+}
+
+// 处理
+func (this *GameListHandler) Process() error {
+	resp := &msg.GameListResponse{Games: []msg.Game{}}
+	app.Sender.Resp(this.Msg.SessionId, this.Msg.GetTopic(), resp)
+	return nil
+}
+
+// 处理后
+func (this *GameListHandler) AfterProcess() {
+}
+
+// 释放
+func (this *GameListHandler) Release() error {
+	return nil
+}
+
+// 错误处理
+func (this *GameListHandler) OnError(stage string, err error) {
+	log.Error(err)
+}
---
+++ simple/server/msg/game_list.go
@@ -0,0 +1,21 @@
+package msg
+
+import "time"
+
+type GameListRequest struct {
+}
+
+type GameListResponse struct {
+	Games []Game `json:"games"`
+}
+
+type Game struct {
+	Id         int       `json:"id"`
+	Name       string    `json:"name"`
+	Desc       string    `json:"desc"`
+	Icon       string    `json:"icon"`
+	Url        string    `json:"url"`
+	Status     int       `json:"status"`
+	CreateTime time.Time `json:"create_time"`
+	UpdateTime time.Time `json:"update_time"`
+}
